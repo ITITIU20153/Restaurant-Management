62efdfbe47f15d64503287dd28a16c5e82ae67ad
Việt Anh <ititiu20153@student.hcmiu.edu.vn>
2026-05-18 19:02:56 +0700

    Update CustomerRepository.java

--- src/main/java/com/vietanh/restaurantmangement/customer/CustomerRepository.java
+++ src/main/java/com/vietanh/restaurantmangement/customer/CustomerRepository.java
@@ -7,13 +7,6 @@ import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    // @Query(value = """
-    // Select customer
-    // From customer
-    // Where username = :userName
-    // """, nativeQuery = true)
-    // Customer findCustomerByUserName(String userName);
-
     boolean existsByUserName(String userName);
 
     @Query(value = """
